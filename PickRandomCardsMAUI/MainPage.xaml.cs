890849771897c3c56bdfe4c8db55d2d57d64c4da
Joe Faith <jfaith@harding.edu>
2025-04-05 21:15:04 -0500

    switched to file-based scoping.

--- PickRandomCardsMAUI/MainPage.xaml.cs
+++ PickRandomCardsMAUI/MainPage.xaml.cs
@@ -1,34 +1,32 @@
-﻿using System;
-using Microsoft.Maui.Controls;
+﻿
 using PickRandomCards;
 
-namespace PickRandomCardsMAUI
+namespace PickRandomCardsMAUI;
+
+public partial class MainPage : ContentPage
 {
-    public partial class MainPage : ContentPage
+    public MainPage()
     {
-        public MainPage()
+        InitializeComponent();
+    }
+
+    private void PickCardsButton_Clicked(object sender, EventArgs e)
+    {
+        if (int.TryParse(NumberOfCards.Text, out int numberOfCards))
         {
-            InitializeComponent();
+            string[] cards = CardPicker.PickSomeCards(numberOfCards);
+            PickedCards.Text = string.Empty; // Clear any previous text
+
+            foreach (string card in cards)
+            {
+                PickedCards.Text += card + Environment.NewLine;
+            }
+
+            PickedCards.Text += Environment.NewLine + "You picked " + numberOfCards + " cards.";
         }
-
-        private void PickCardsButton_Clicked(object sender, EventArgs e)
+        else
         {
-            if (int.TryParse(NumberOfCards.Text, out int numberOfCards))
-            {
-                string[] cards = CardPicker.PickSomeCards(numberOfCards);
-                PickedCards.Text = string.Empty; // Clear any previous text
-
-                foreach (string card in cards)
-                {
-                    PickedCards.Text += card + Environment.NewLine;
-                }
-
-                PickedCards.Text += Environment.NewLine + "You picked " + numberOfCards + " cards.";
-            }
-            else
-            {
-                PickedCards.Text = "Please enter a valid number.";
-            }
+            PickedCards.Text = "Please enter a valid number.";
         }
     }
 }
