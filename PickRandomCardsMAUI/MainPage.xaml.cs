46fd52161cba95d8c5db73450e61fec5ec79615c
Joe Faith <jfaith@harding.edu>
2025-04-05 20:35:42 -0500

    Add project files.

---
+++ PickRandomCardsMAUI/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+using PickRandomCards;
+
+namespace PickRandomCardsMAUI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void PickCardsButton_Clicked(object sender, EventArgs e)
+        {
+            if (int.TryParse(NumberOfCards.Text, out int numberOfCards))
+            {
+                string[] cards = CardPicker.PickSomeCards(numberOfCards);
+                PickedCards.Text = string.Empty; // Clear any previous text
+
+                foreach (string card in cards)
+                {
+                    PickedCards.Text += card + Environment.NewLine;
+                }
+
+                PickedCards.Text += Environment.NewLine + "You picked " + numberOfCards + " cards.";
+            }
+            else
+            {
+                PickedCards.Text = "Please enter a valid number.";
+            }
+        }
+    }
+}
